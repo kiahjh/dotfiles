06e291f6f2e58b7e20765acb5a2987e7078eaaca
Jared Henderson <jaredh159@users.noreply.github.com>
2022-11-02 12:00:18 -0400

    vim: disable tsserver "suggestions"

---
+++ nvim/.config/nvim/lua/user/lsp/settings/tsserver.lua
@@ -0,0 +1,10 @@
+return {
+  init_options = {
+    -- disable ts suggestions, especially "convert to es module"
+    -- see https://stackoverflow.com/a/70294761/208770
+    -- long-term, would be great to filter diagnostics by code
+    -- as was done in this archived plugin:
+    -- https://githubcom/jose-elias-alvarez/nvim-lsp-ts-utils
+    preferences = { disableSuggestions = true },
+  },
+}
