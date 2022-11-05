503302296f2b364b71f5aeee487965fba0b55f48
Jared Henderson <jaredh159@users.noreply.github.com>
2022-11-05 17:17:21 -0400

    vim: fix searching for stuff in .<dir>s

--- nvim/.config/nvim/lua/user/telescope.lua
+++ nvim/.config/nvim/lua/user/telescope.lua
@@ -4,6 +4,16 @@ if not status_ok then
 end
 
 telescope.setup({
+	require("telescope").setup({
+		pickers = {
+			live_grep = {
+				additional_args = function()
+					-- https://github.com/BurntSushi/ripgrep/issues/340#issuecomment-280868301
+					return { "--hidden", "-g", "!.git/", "-g", "!node_modules/", "-g", "!.oh-my-zsh/" }
+				end,
+			},
+		},
+	}),
 	defaults = {
 		selection_caret = " ",
 		path_display = { "smart" },
