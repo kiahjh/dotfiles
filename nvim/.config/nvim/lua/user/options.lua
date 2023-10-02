4c2bce697168b0793737ccb16e1a5aebba697b31
Miciah Henderson <48624788+kiahjh@users.noreply.github.com>
2023-10-02 09:22:24 -0400

    nvim: made '-' character not considered part of a word and added linewrapping

--- nvim/.config/nvim/lua/user/options.lua
+++ nvim/.config/nvim/lua/user/options.lua
@@ -29,7 +29,7 @@ local options = {
 	relativenumber = false, -- set relative numbered lines
 	numberwidth = 4, -- set number column width to 2 {default 4}
 	signcolumn = "yes", -- always show the sign column, otherwise it would shift the text each time
-	wrap = false, -- display lines as one long line
+	wrap = true, -- break lines (tailwindcss)
 	scrolloff = 8, -- is one of my fav
 	sidescrolloff = 8,
 	guifont = "monospace:h17", -- the font used in graphical neovim applications
@@ -42,5 +42,4 @@ for k, v in pairs(options) do
 end
 
 vim.cmd("set whichwrap+=<,>,[,],h,l")
-vim.cmd([[set iskeyword+=-]])
 vim.cmd([[set formatoptions-=cro]]) -- TODO: this doesn't seem to work
