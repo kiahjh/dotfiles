cce73028ec0dc1145566dcf34f461fa3675f9be6
Miciah Henderson <48624788+kiahjh@users.noreply.github.com>
2024-01-24 10:05:14 -0500

    nvim: better alpha dash

--- nvim/.config/nvim/lua/kiahjh/plugins/alpha-nvim.lua
+++ nvim/.config/nvim/lua/kiahjh/plugins/alpha-nvim.lua
@@ -1,37 +1,48 @@
 return {
-	"goolord/alpha-nvim",
-	event = "VimEnter",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-	config = function()
-		local alpha = require("alpha")
-		local dashboard = require("alpha.themes.dashboard")
-
-		-- Set header
-		dashboard.section.header.val = {
-			"                                                     ",
-			"  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
-			"  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
-			"  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
-			"  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
-			"  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
-			"  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
-			"                                                     ",
-		}
-
-		-- Set menu
-		dashboard.section.buttons.val = {
-			dashboard.button("e", "  > New File", "<cmd>ene<CR>"),
-			dashboard.button("SPC ee", "  > Toggle file explorer", "<cmd>NvimTreeToggle<CR>"),
-			dashboard.button("SPC wr", "󰁯  > Restore Session For Current Directory", "<cmd>SessionRestore<CR>"),
-			dashboard.button("SPC ff", "󰱼  > Find File", "<cmd>Telescope find_files<CR>"),
-			dashboard.button("SPC fs", "  > Find Word", "<cmd>Telescope live_grep<CR>"),
-			dashboard.button("q", " > Quit NVIM", "<cmd>qa<CR>"),
-		}
-
-		-- Send config to alpha
-		alpha.setup(dashboard.opts)
-
-		-- Disable folding on alpha buffer
-		vim.cmd([[autocmd FileType alpha setlocal nofoldenable]])
-	end,
+	{
+		"goolord/alpha-nvim",
+		event = "VimEnter",
+		opts = function()
+			local dashboard = require("alpha.themes.dashboard")
+			local logo = [[
+                                             
+      ████ ██████           █████      ██
+     ███████████             █████ 
+     █████████ ███████████████████ ███   ███████████
+    █████████  ███    █████████████ █████ ██████████████
+   █████████ ██████████ █████████ █████ █████ ████ █████
+ ███████████ ███    ███ █████████ █████ █████ ████ █████
+██████  █████████████████████ ████ █████ █████ ████ ██████
+]]
+			dashboard.section.header.val = vim.split(logo, "\n")
+			dashboard.section.buttons.val = {
+				dashboard.button("e", "  > New File", "<cmd>ene<CR>"),
+				dashboard.button("SPC ee", "  > Toggle file explorer", "<cmd>NvimTreeToggle<CR>"),
+				dashboard.button("SPC wr", "󰁯  > Restore Session For Current Directory", "<cmd>SessionRestore<CR>"),
+				dashboard.button("SPC ff", "󰱼  > Find File", "<cmd>Telescope find_files<CR>"),
+				dashboard.button("SPC fs", "  > Find Word", "<cmd>Telescope live_grep<CR>"),
+				dashboard.button("q", "  > Quit NVIM", "<cmd>qa<CR>"),
+			}
+			dashboard.section.header.opts.hl = "AlphaHeader"
+			dashboard.opts.layout[1].val = 6
+			return dashboard
+		end,
+		config = function(_, dashboard)
+			require("alpha").setup(dashboard.opts)
+			vim.api.nvim_create_autocmd("User", {
+				callback = function()
+					local stats = require("lazy").stats()
+					local ms = math.floor(stats.startuptime * 100) / 100
+					dashboard.section.footer.val = "󱐌 Lazy-loaded "
+						.. stats.loaded
+						.. "/"
+						.. stats.count
+						.. " plugins in "
+						.. ms
+						.. "ms"
+					pcall(vim.cmd.AlphaRedraw)
+				end,
+			})
+		end,
+	},
 }
