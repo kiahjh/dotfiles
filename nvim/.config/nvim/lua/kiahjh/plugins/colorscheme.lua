538c9aa26109214f3a3dc8a9412e1fbbc1e98af0
Miciah Henderson <48624788+kiahjh@users.noreply.github.com>
2024-02-02 14:32:52 -0500

    nvim and tmux: switched to catppuchin-mocha theme and got italics working in tmux (finally)

--- nvim/.config/nvim/lua/kiahjh/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/kiahjh/plugins/colorscheme.lua
@@ -1,21 +1,58 @@
 return {
-	-- {
-	-- 	"bluz71/vim-nightfly-guicolors",
-	-- 	priority = 1000, -- make sure to load this before all the other start plugins
-	-- 	config = function()
-	-- 		-- load the colorscheme here
-	-- 		vim.cmd([[colorscheme nightfly]])
-	-- 	end,
-	-- },
 	{
-		"folke/tokyonight.nvim",
+		"catppuccin/nvim",
 		priority = 1000, -- make sure to load this before all the other start plugins
 		config = function()
-			require("tokyonight").setup({
-				style = "night",
+			require("catppuccin").setup({
+				flavour = "mocha", -- latte, frappe, macchiato, mocha
+				background = { -- :h background
+					light = "latte",
+					dark = "mocha",
+				},
+				transparent_background = false, -- disables setting the background color.
+				show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
+				term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
+				dim_inactive = {
+					enabled = true, -- dims the background color of inactive window
+					shade = "dark",
+					percentage = 0.15, -- percentage of the shade to apply to the inactive window
+				},
+				no_italic = false, -- Force no italic
+				no_bold = false, -- Force no bold
+				no_underline = false, -- Force no underline
+				styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+					comments = { "italic" }, -- Change the style of comments
+					conditionals = {},
+					loops = {},
+					functions = {},
+					keywords = { "italic" },
+					strings = { "italic" },
+					variables = { "bold" },
+					numbers = {},
+					booleans = {},
+					properties = {},
+					types = { "italic" },
+					operators = {},
+				},
+				color_overrides = {},
+				custom_highlights = {},
+				integrations = {
+					cmp = true,
+					gitsigns = true,
+					nvimtree = true,
+					treesitter = true,
+					notify = true,
+					alpha = true,
+					harpoon = true,
+					mini = {
+						enabled = true,
+						indentscope_color = "",
+					},
+					-- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+				},
 			})
 			-- load the colorscheme here
-			vim.cmd([[colorscheme tokyonight]])
+			vim.cmd([[colorscheme catppuccin]])
 		end,
 	},
 }
